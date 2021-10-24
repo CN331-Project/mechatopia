86a58ac788d4ab5e18aeae91926d732ad51b3439
pornchai phumphinyo <6210612732@student.tu.ac.th>
2021-10-24 13:34:00 +0000

    heroku up_05

--- mechatopia/mechatopia/settings.py
+++ mechatopia/mechatopia/settings.py
@@ -122,12 +122,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR /'static']
+#MEDIA_URL = '/media/'
+#STATICFILES_DIRS = [BASE_DIR /'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#import django_heroku
+#django_heroku.settings(locals())
