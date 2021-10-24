0367805ed6331b94932689329d32634fdc4f539f
pornchai phumphinyo <6210612732@student.tu.ac.th>
2021-10-24 13:06:41 +0000

    heroku up_01

--- mechatopia/activate_env.py
+++ mechatopia/activate_env.py
@@ -8,6 +8,7 @@ os.system('cmd /k ' + pp)
 os.system(qq)
 
 
+
 #print('getcwd:      ', os.getcwd())
 #print('__file__:    ', __file__)
 #os.system('cmd /k "C:\\Users\\Administrator\\Desktop\\cn331_project_mechatopia\\.venv_project\\Scripts\\activate"' tesdewettt ttt tt)
